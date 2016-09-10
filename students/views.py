c07edd0d3b97b1b80de58188a52833af49a5c50e
Evgeny Krasyuk <evgeniykrasyuk@gmail.com>
2016-09-10 21:52:44 +0300

    Add inheritance in students_list.html and groups_list_html from base.html

--- students/views.py
+++ students/views.py
@@ -40,7 +40,21 @@ def students_delete(request, sid):
 #Views for Groups
 
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	groups = (
+		{'id': 1,
+		 'name': 'МтМ-21',
+		 'leader': {'id': '1', 'name': 'Ячменєв Олег'}},
+
+		 {'id': 2,
+		  'name': 'МтМ-22',
+		  'leader': {'id': '2', 'name': 'Віталій Подоба'}},
+
+		  {'id': 3,
+		   'name': 'МтМ-23',
+		   'leader': {'id': '3', 'name': 'Іванов Андрій'}},
+		)
+		
+	return render(request, 'students/groups_list.html', {'groups': groups})
 
 def groups_add(request):
 	return HttpResponse('<h1>Group Add Form</h1>')
