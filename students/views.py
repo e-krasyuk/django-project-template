f079f8e18e479dfcc9d424a4c9dc9ac255cc086c
Evgeny Krasyuk <evgeniykrasyuk@gmail.com>
2016-09-10 13:23:09 +0300

    add dictionary with values for students

--- students/views.py
+++ students/views.py
@@ -1,10 +1,32 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 #Views for Students
 
 def students_list(request):
-		return render(request, 'students/students_list.html', {})
+		students = (
+			{'id': 1,
+			 'first_name': u'Корост',
+			 'last_name': u'Андрій',
+			 'ticket': 2123,
+			 'image': 'img/me.jpeg'},
+
+			{'id': 2,
+			 'first_name': u'Шевченко',
+			 'last_name': u'Сергій',
+			 'ticket': 3862,
+			 'image': 'img/podoba3.jpg'},
+
+			{'id': 3,
+			 'first_name': u'Левченко',
+			 'last_name': u'Олексій',
+			 'ticket': 1363,
+			 'image': 'img/piv.png'},
+		)
+			
+		return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
