364421e0089646330295236ee50a724924db33b6
Evgeny Krasyuk <evgeniykrasyuk@gmail.com>
2016-12-07 17:55:24 +0200

    journal page navigation debug

--- students/util.py
+++ students/util.py
@@ -1,15 +1,17 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def paginate(objects, size, request, context, var_name='object_list'):
-	"""Paginate objects provided by view.
+	'''Paginate objects provided by view.
+
 	This function takes:
-		* list of elements;
-		* number of objects per page;
-		* request object to get url parameters from;
-		* context to set new variables into;
-		* var_name - variable name for list of objects.
+	* list of elements;
+	* number of objects per page;
+	* request object to get url parameters from;
+	* context to set new variables into;
+	* var_name - variable name for list of objects.
+	
 	It returns updated context object.
-	"""
+	'''
 	# apply pagination
 	paginator = Paginator(objects, size)
 
@@ -24,14 +26,14 @@ def paginate(objects, size, request, context, var_name='object_list'):
 		# if page is out of range (e.g. 9999),
 		# deliver last page of results
 		object_list = paginator.page(paginator.num_pages)
-		
 	# set variables into context
 	context[var_name] = object_list
 	context['is_paginated'] = object_list.has_other_pages()
 	context['page_obj'] = object_list
 	context['paginator'] = paginator
-	
+
 	return context
+
 
 def get_groups(request):
 	'''Ruturns list of existing groups'''
